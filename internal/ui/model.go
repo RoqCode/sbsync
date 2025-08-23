da677d355705c78a25c3ae60bc9a915482109363
Hajo Haas <hajo.haas@hmmh.de>
2025-08-24 00:39:09 +0200

    feat: prefix list items based on type

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -20,10 +20,10 @@ var (
 	focusStyle   = lipgloss.NewStyle().Bold(true)
 	selStyle     = lipgloss.NewStyle().Reverse(true)
 
-        // markers for different story types (colored squares)
-        symbolStory  = lipgloss.NewStyle().Background(lipgloss.Color("111")).Render(" ")
-        symbolFolder = lipgloss.NewStyle().Background(lipgloss.Color("214")).Render(" ")
-        symbolRoot   = lipgloss.NewStyle().Background(lipgloss.Color("82")).Render(" ")
+	// markers for different story types (colored squares)
+	symbolStory  = lipgloss.NewStyle().Foreground(lipgloss.Color("#8942E1")).Render("S")
+	symbolFolder = lipgloss.NewStyle().Foreground(lipgloss.Color("#3AC4BA")).Render("F")
+	symbolRoot   = lipgloss.NewStyle().Foreground(lipgloss.Color("214")).Render("R")
 )
 
 // --- Model / State ---
