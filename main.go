abae56f91216987200e56b79ef3586475293032c
Hajo Haas <hajo.haas@hmmh.de>
2025-08-23 07:42:41 +0200

    feat: basic auth + install dependencies

--- main.go
+++ main.go
@@ -1,7 +1,96 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var TOKEN_PATH = "./.sbrc"
+
+type StoryblokClient struct {
+	Token      string
+	httpClient *http.Client
+}
+
+type SpacesResponse struct {
+	Spaces []Space `json:"spaces"`
+}
+
+type Space struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Region string `json:"region"`
+}
+
+func (c *StoryblokClient) ListSpaces() {
+	req, err := http.NewRequest("GET", "https://mapi.storyblok.com/v1/spaces/", nil)
+	if err != nil {
+		fmt.Println("[ListSpaces]: error building request:", err)
+		return
+	}
+
+	req.Header.Set("Authorization", c.Token)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		fmt.Println("[ListSpaces]: request error:", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	fmt.Println("[ListSpaces] status:", resp.Status)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("[ListSpaces]: error reading body:", err)
+		return
+	}
+
+	var out SpacesResponse
+	err = json.Unmarshal(body, &out)
+	if err != nil {
+		fmt.Println("[ListSpaces]: error parsing body:", err)
+		return
+	}
+
+	for _, s := range out.Spaces {
+		fmt.Printf("- %s (id=%d, region=%s)\n", s.Name, s.ID, s.Region)
+	}
+}
+
+func NewStoryblokClient(token string) *StoryblokClient {
+	return &StoryblokClient{
+		Token:      token,
+		httpClient: &http.Client{},
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	rcFile, err := os.Open(TOKEN_PATH)
+	if err != nil {
+		fmt.Println("[file reader]: error finding rc:", err)
+		return
+	}
+	defer rcFile.Close()
+
+	rcContent, err := io.ReadAll(rcFile)
+	if err != nil {
+		fmt.Println("[file reader]: error reading rc:", err)
+		return
+	}
+
+	parts := strings.SplitN(string(rcContent), "=", 2)
+	if len(parts) != 2 {
+		fmt.Println("[file reader]: invalid format in rc file")
+		return
+	}
+	token := strings.TrimSpace(parts[1])
+
+	client := NewStoryblokClient(token)
+	client.ListSpaces()
 }
